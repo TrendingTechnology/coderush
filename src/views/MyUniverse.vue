4a61558cacab8cfd1e29c659ff51fd8cd7947724
encap <encapsulation4@gmail.com>
2020-05-19 00:20:22 +0200

    fix

--- src/views/MyUniverse.vue
+++ src/views/MyUniverse.vue
@@ -9,7 +9,7 @@
         The problem is that I'm usually doing it alone (what a suprise) or when I'm with someone, there is just no time for setting up the camera. So you must take my word for it.
       </p>
       <p />
-      <p>I'm currently rebuiliding whole layout. If something is overflowing or not visible, try zooming out to 80% in your browser.</p>
+      <p>I'm currently rebuiliding whole layout. Everything after that works, but not for everyone. If something is overflowing or not visible, try zooming out to 80% in your browser.</p>
       <p />
     </main>
     <footer>
